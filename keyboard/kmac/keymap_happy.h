bdb3e3b10e8328d52cb2c84582c45e59a438b65f
Hans <h.viktorin@gmail.com>
2016-02-18 19:15:35 +0000

    modified happy keymap

--- keyboard/kmac/keymap_happy.h
+++ keyboard/kmac/keymap_happy.h
@@ -6,21 +6,29 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
         LCTL,     A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT, \
         LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
-        FN1,LGUI,LALT,             SPC,                     RALT,RGUI,RCTL ),
+        FN2,LGUI,LALT,             SPC,                     RALT,FN1,RCTL ),
     [1] = KEYMAP_HAPPY(\
         ESC,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,F12,GRV,BSLS, \
         CAPS, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   UP,RBRC,DEL, \
         LCTL, A,   S,   D,   F,   G,   H,   J,   K,   L,   LEFT,RIGHT,     ENT, \
         LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, DOWN,RSFT,FN0, \
-        FN1,LGUI,LALT,               SPC,                     RALT,RGUI,RCTL ),
+        FN2,LGUI,LALT,               SPC,                     RALT,FN1,RCTL ),
     [2] = KEYMAP_HAPPY(\
         ESC,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,F12,GRV,BSLS, \
-        CAPS, NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  RBRC,DEL, \
-        LCTL, VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,     ENT, \
-        LSFT, NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,DOWN,RSFT,FN0, \
-        FN1,LGUI,LALT,               SPC,                     RALT,RGUI,RCTL ),
+        CAPS, NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,PGUP,  RBRC,DEL, \
+        LCTL, VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,HOME,END,     ENT, \
+        LSFT, NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,PGDN,RSFT,FN0, \
+        FN2,LGUI,LALT,               SPC,                     RALT,FN1,RCTL ),
+    [3] = KEYMAP_HAPPY(\
+        ESC,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,F12,GRV,BSLS, \
+        CAPS, NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,PGUP,  RBRC,DEL, \
+        LCTL, VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,HOME,END,     ENT, \
+        LSFT, NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,PGDN,RSFT,FN0, \
+        FN2,LGUI,LALT,               SPC,                     RALT,FN1,RCTL ),
+
 };
 static const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
     [1] = ACTION_LAYER_MOMENTARY(2),
+    [2] = ACTION_LAYER_MOMENTARY(3),
 };
