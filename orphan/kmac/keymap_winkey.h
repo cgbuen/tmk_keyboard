550f99e8421e9db0ca1fd18a67d7fa742c96d6ec
ageaenes <h.viktorin@gmail.com>
2016-09-01 00:16:11 +0200

    dvorak on ansi

--- orphan/kmac/keymap_winkey.h
+++ orphan/kmac/keymap_winkey.h
@@ -3,10 +3,10 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP(\
         ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR,SLCK,BRK,  \
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,      INS, HOME,PGUP, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,      DEL, END, PGDN, \
-        FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                       \
-        LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,           UP,        \
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   SLSH, EQL,BSLS,      INS, HOME,PGUP, \
+        TAB,QUOT,COMM,DOT,   P,   Y,   F,   G,   C   ,R   ,L  ,LBRC,RBRC,BSPC,      DEL, END, PGDN, \
+        LCTL, A,   O,   E,   U,   I,   D,   H,   T,   N,   S,  MINS,     ENT,                       \
+        LSFT, SCLN,   Q,   J,   K,   X,   B,   M,   W,    V,     Z,      RSFT,           UP,        \
         LCTL,LGUI,LALT,               SPC,                RALT,RGUI,APP, RCTL,      LEFT,DOWN,RGHT),
     /* 1: media keys */
     KEYMAP(\
