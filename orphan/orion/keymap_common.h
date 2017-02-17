9601eef69a20df66e0b7139110a082646cf870a2
ageaenes <h.viktorin@gmail.com>
2017-02-17 12:21:53 +0100

    tidy up

--- orphan/orion/keymap_common.h
+++ orphan/orion/keymap_common.h
@@ -37,7 +37,7 @@ const action_t fn_actions[];
 /* 4 */   { KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K4E, KC_##K4F, KC_##K4G, KC_##K4H, KC_##K4I, KC_##K4J, KC_##K4K, KC_##K4L, KC_##K4M, KC_##K4N, KC_##K4O, KC_##K4P, KC_##K4Q, KC_##K4R}, \
 /* 3 */   { KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D, KC_##K3E, KC_##K3F, KC_##K3G, KC_##K3H, KC_##K3I, KC_##K3J, KC_##K3K, KC_##K3L, KC_##K3M, KC_##K3N, KC_NO,    KC_##K3P, KC_##K3Q, KC_##K3R}, \
 /* 2 */   { KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D, KC_##K2E, KC_##K2F, KC_##K2G, KC_##K2H, KC_##K2I, KC_##K2J, KC_##K2K, KC_##K2L, KC_NO,    KC_##K2N, KC_NO,    KC_NO,    KC_NO,    KC_NO   }, \
-/* 1 */   { KC_##K1A,           KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_##K1L, KC_NO,    KC_##K1N, KC_##K1O, KC_NO,    KC_NO,    KC_##K1R   }, \
+/* 1 */   { KC_##K1A,           KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_##K1L, KC_NO,    KC_##K1N, KC_##K1O, KC_NO,    KC_NO,    KC_##K1R}, \
 /* 0 */   { KC_##K0A, KC_##K0B, KC_##K0C, KC_NO,    KC_NO,    KC_##K0G, KC_NO,    KC_NO,    KC_##K0K, KC_NO,    KC_##K0L, KC_NO,    KC_##K0M, KC_##K0N, KC_NO,    KC_##K0P, KC_##K0Q, KC_##K0R}  \
 }
 
