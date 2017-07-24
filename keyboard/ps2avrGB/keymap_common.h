15c24570dcb12701ce3974a9c98a9f4f8a660108
ageaenes <h.viktorin@gmail.com>
2017-07-25 01:06:26 +0200

    changes for function buttons s

--- keyboard/ps2avrGB/keymap_common.h
+++ keyboard/ps2avrGB/keymap_common.h
@@ -37,12 +37,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     K04, K14, K24, K34, K44, K54, K16, KB6, KB7, K17, KA4, KB4, KC4, KD4, KE4, KD0, \
     K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3,      K67, \
     K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KB2, KD2,           KE0, \
-    K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1,           K86, K77, \
+    K01, K30, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, KC1,      K86, K77, \
     K00, K10, K20,           K56,                     K57, KB0, KC0, K66, K76, K96  \
 ) \
 { \
-    { KC_##K00, KC_##K10, KC_##K20, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KB0, KC_##KC0, KC_##KD0, KC_##KE0 }, \
-    { KC_##K01, KC_##K11, KC_##K21, KC_##K31, KC_##K41, KC_##K51, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KA1, KC_##KB1, KC_NO,    KC_##KD1, KC_##KE1 }, \
+    { KC_##K00, KC_##K10, KC_##K20, KC_##K30, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KB0, KC_##KC0, KC_##KD0, KC_##KE0 }, \
+    { KC_##K01, KC_##K11, KC_##K21, KC_##K31, KC_##K41, KC_##K51, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KA1, KC_##KB1, KC_##KC1,    KC_##KD1, KC_##KE1 }, \
     { KC_##K02, KC_##K12, KC_##K22, KC_##K32, KC_##K42, KC_##K52, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KA2, KC_##KB2, KC_NO,    KC_##KD2, KC_##KE2 }, \
     { KC_##K03, KC_##K13, KC_##K23, KC_##K33, KC_##K43, KC_##K53, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KA3, KC_##KB3, KC_##KC3, KC_##KD3, KC_NO    }, \
     { KC_##K04, KC_##K14, KC_##K24, KC_##K34, KC_##K44, KC_##K54, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KA4, KC_##KB4, KC_##KC4, KC_##KD4, KC_##KE4 }, \
@@ -55,14 +55,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     K04, K14, K24, K34, K44, K54, K16, KB6, KB7, K17, KA4, KB4, KC4, KD4, KE4, \
     K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3,     \
     K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KB2, KD2,                 \
-    K01, K06 ,K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, K77, \
+    K01, K30 ,K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, KC1, \
     K00, K10, K20,           K56,                     K57, KB0, KC0 \
 ) KEYMAP( \
-    NO,  NO,  NO,  NO,  NO,  K06,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO, NO, \
+    NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO, NO, \
     K04, K14, K24, K34, K44, K54, K16, KB6, KB7, K17, KA4, KB4, KC4, KD4, KE4, NO, \
     K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3,      NO, \
     K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KB2, KD2,           NO, \
-    K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, NO, K77, \
+    K01, K30, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, KC1, NO, NO, \
     K00, K10, K20,           K56,                     K57, KB0, KC0, NO, NO, NO  \
 )
 
--- keyboard/ps2avrGB/keymap_ps2avrGB.c
+++ keyboard/ps2avrGB/keymap_ps2avrGB.c
@@ -6,21 +6,21 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   SLSH,EQL,GRV,BSLS,       \
         TAB, QUOT, COMM,   DOT,   P,   Y,   F,   G,   C,   R,   L, LBRC, RBRC, BSPC,    \
         LCTL, A,   O,   E,   U,   I,   D,   H,   T,   N,   S, MINS,     ENT,            \
-        LSFT, A, SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT, B,\
+        LSFT, DEL, SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT, FN0,\
         NO, LGUI,LALT,          FN0,                     RGUI,RALT,NO\
     ),
     KEYMAP_BFACE( \
         ESC, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,   F11, F12 ,GRV,BSLS, \
         TAB, FN3, FN4,   FN5,   P,   Y,   F,   G,   UP,   R,   L, LBRC, RBRC, DEL,\
         LCTL, A,   O,   E,   U,   I,   D,   LEFT,DOWN,RIGHT,   S, MINS,     ENT,         \
-        FN1, A,SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT,          B, \
+        LSFT, FN1,SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT,          FN0, \
         TRNS,TRNS,TRNS,          FN0,                      TRNS,TRNS,TRNS \
     ),
     KEYMAP_BFACE( \
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   SLSH,EQL,GRV,BSLS, \
         TAB, FN6, FN7,   FN8,   P,   Y,   F,   G,   PGUP,   R,   L, LBRC, RBRC, BSPC, \
         LCTL, A,   O,   E,   U,   I,   D,   HOME,   PGDN,   END,   S, MINS,     ENT,       \
-        FN1, A,SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT,    B,\
+        LSFT, FN1,SCLN, Q,  J,   K,   X,   B,   M,   W,   V, Z,       RSFT,    FN0,\
         TRNS,TRNS,TRNS,          FN0,                      TRNS,TRNS,TRNS\
     ),
 };
