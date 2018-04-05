31dd9764ff404b7d479b92c184ea6b635ae6a005
henz <hans@qrowdbuy.com>
2018-04-05 14:43:59 +0000

    modifications to winkeyless keymap

--- orphan/kmac/keymap_winkeyless.h
+++ orphan/kmac/keymap_winkeyless.h
@@ -24,6 +24,13 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL, A,   O,   E,   U,   I,   D,   LEFT,DOWN,RIGHT,   S, MINS,     ENT,                      \
         FN1,     TRNS,TRNS,CALC,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     CAPS,           TRNS,      \
         TRNS,TRNS,TRNS,               FN0,                    TRNS,TRNS,TRNS,      TRNS,TRNS,TRNS),
+    KEYMAP_WINKEYLESS(\
+        ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR,SLCK,BRK,  \
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   SLSH, EQL,BSLS,      INS, HOME,PGUP, \
+        TAB,QUOT,COMM,DOT,   P,   Y,   F,   G,   C   ,R   ,L  ,LBRC,RBRC,BSPC,      DEL, END, PGDN, \
+        LCTL, A,   O,   E,   U,   I,   D,   H,   T,   N,   S,  MINS,     ENT,                       \
+        LSFT, SCLN,   Q,   J,   K,   X,   B,   M,   W,    V,     Z,      RSFT,           UP,        \
+        LGUI,NO,LALT,               SPC,                     RALT,NO,RCTL,      LEFT,DOWN,RGHT),
 };
 static const action_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TAP_KEY(1, KC_SPC),
